30fc71a8c4b428d8c51fe089a502b47d56600978
William Martin <williammartincruz@gmail.com>
2014-07-02 18:36:25 -0500

    completed assignment

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php
 
-phpinfo();
+<?php
+
+print 'hello world!br />';
+
+for($i=1;$i<6;$i++)
+{
+    print "hello jesse";
+    print "hey $i tjmes";
+    print "yo!";
+    print "hey $i tjmes";
+    print "yo!";
+}
+
+?>
